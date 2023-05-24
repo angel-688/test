d66017ef46cb760a071f5b6fd2fa5bcea606b687
angel-688 <64227644+angel-688@users.noreply.github.com>
2023-05-24 13:52:05 +0300

    Add files via upload

--- telegram.php
+++ telegram.php
@@ -1,31 +1,19 @@
 <?php
-$name = $_POST [' user _name '];
+$name = $_POST ['user_name'];
 $token = "5674676719:AAEKyLdE2JadnPQH5lyamYgo46OhJWOjR_4";
 $Schat id = "-968208666";
 $arr = array(
- 'Имя пользователя: [' => $name,
+ 'Имя пользователя:' => $name
 );
 
-foreach ($arr as $key =› $value) { $txt .=
-"‹b>" $key. "‹/b> " $value. "%OA";
-];
+foreach ($arr as $key =› $value) { 
+	$txt .="‹b>".$key."‹/b> ".$value."%OA";
+};
 
-$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=htmI&text=($txt]", "r");
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=htmI&text=($txt]","r");
 
 if ($sendToTelegram) {
 echo '<h1 class="success"›Спасибо за отправку вашего сообщения!</h1>';
 return true;
-} else {
-header ('Location: thank-you. html');
-}
-
-server {
-listen 80;
-server_name localhost;
-location / {
-root html;
-index index.html index.htm;
-}
-error_page 405 =200 $uri;
 }
 ?>
