60cae9d995d7e554733c901439e6cbc819aec6b3
angel-688 <64227644+angel-688@users.noreply.github.com>
2023-05-24 15:08:41 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,34 +1,19 @@
 <?php
 $name = $_POST['user_name'];
-/*функция для создания запроса на сервер Telegram */
-function parser($url){
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    $result = curl_exec($curl);
-    if($result == false){     
-      echo "Ошибка отправки запроса: " . curl_error($curl);
-      return false;
-    }
-    else{
-      return true;
-    }
+$token = "5674676719:AAEKyLdE2JadnPQH5lyamYgo46OhJWOjR_4";
+$chat_id = "-968208666";
+$arr = array('Имя пользователя:' => $name);
+
+$txt = '';
+foreach ($arr as $key => $value) { 
+	 $txt .= "<b>".$key."</b> ".$value."\n";
 }
 
-/*собираем сообщение*/
-function orderSendTelegram($message) {
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text=($txt)","r");
 
-    /*токен который выдаётся при регистрации бота */
-    $token = "5674676719:AAEKyLdE2JadnPQH5lyamYgo46OhJWOjR_4";
-    /*идентификатор группы*/
-    $chat_id = "-968208666";
-
-    /*делаем запрос*/
-    parser("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$message}");
-
+if ($sendToTelegram) {
+	 echo '<h1 class="success">Спасибо за отправку вашего сообщения!</h1>';
+	 return true;
 }
-$textMessage .= '<b> User name: </b>'.$name.'/n';
-$textMessage .= urldecode($textMessage);
-orderSendTelegram($textMessage);
-
 ?>
+
